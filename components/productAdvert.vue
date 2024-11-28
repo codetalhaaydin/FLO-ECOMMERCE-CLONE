6689a6ed679783952daeff9841832f9c29fb7bdb
codetalhaaydin <talhaaydin02516@gmail.com>
2024-11-28 21:14:10 +0300

    app.vue guncellemesi

--- components/productAdvert.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>Ürün ilanı</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style></style>
--- components/recomendeds.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>Tavsiye Edilenler</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style></style>
