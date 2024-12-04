1d161d7615464600cbabfd8fc22ff8b1da15e736
Nazar-04 <nazarnznz@gmail.com>
2024-12-04 10:16:11 +0300

    header  duzeltmesi

--- components/header.vue
+++ components/header.vue
@@ -41,10 +41,12 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
   name: "Header",
-};
+});
 </script>
 
 <style scoped>
