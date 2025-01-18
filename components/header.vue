d35e45b5055bbefbdef74517ecac4958eef233de
codetalhaaydin <talhaaydin02516@gmail.com>
2025-01-18 17:42:58 +0300

    yardimSayfasi eklemeler

--- components/header.vue
+++ components/header.vue
@@ -41,12 +41,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
+<script>
+export default {
   name: "Header",
-});
+};
 </script>
 
 <style scoped>
